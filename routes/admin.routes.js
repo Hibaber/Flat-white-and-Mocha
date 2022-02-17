9c4697d5e0f48e2ca437443af81b6f6db6d47c83
Hibaber <hiba.berber@gmail.com>
2022-02-17 13:33:31 +0100

    thursday

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,86 +1,121 @@
+
 const router = require("express").Router()
 const { isLoggedIn, checkRole } = require("../middleware/route-guard");
 const UserModel = require("../models/User.model");
 const { isAdmin } = require('../utils')
 const fileUploader = require('../config/cloudinary.config');
+const Cafeteria = require('../models/Cafeteria.model')
 
 
 
 // Perfil administrador
-
 router.get('/admin-profile', isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
-
   res.render('admin/profile_page')
 })
 
 
-// privileges to the Admin
+//Cafeteria
+//Crear nueva cafeteria
+//GET
+router.get('/create-cafeteria', isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
+  res.render('cafeteria/create_form')
+});
 
-//CAFETERIA
+//POST
+router.post('/create-cafeteria', fileUploader.single('image'), isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
 
-//edit 
+  const { name, type, lat, lng, description, transport, website, rating } = req.body
 
-router.get("/edit/:id/cafeteria", isLoggedIn, (req, res, next) => {
+  Cafeteria
+    .create({ name, type, image: req.file.path, location: { coordinates: [lat, lng] }, description, transport, website, rating })
+    .then(() => {
+      return Cafeteria.find()
+    })
+    .then(list => res.render('cafeteria/list_page'))
+    .catch(err => console.log(err))
+})
+// Editar cafeteria
+// GET
+router.get("/edit/:id/cafeteria", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   const { id } = req.params;
+
   Cafeteria
     .findById(id)
     .then((cafeteria) => res.render("cafeteria/edit_form", cafeteria))
     .catch((err) => console.log(err));
 });
-
-router.post("/edit/:id/cafeteria", isLoggedIn, fileUploader.single('image'), (req, res, next) => {
+// POST
+router.post("/edit/:id/cafeteria", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   const { id } = req.params;
   const { name, type, lat, lng, description, transport, website, rating } = req.body;
 
   Cafeteria
-    .findByIdAndUpdate(id, { name, type, image: req.file.path, location: { coordinates: [lat, lng] }, description, transport, website, rating })
-    .then(() => res.redirect("cafeteria/list_page"))
+    .findByIdAndUpdate(id, { name, type, description, transport, website, rating })
+    .then(() => res.redirect("/cafeteria/list_page"))
     .catch((err) => console.log(err));
 });
 
-//delete
-
-router.post("/delete/:id/cafeteria", isLoggedIn, (req, res, next) => {
+//Borrar cafeteria
+//POST
+router.post("/delete/:id/cafeteria", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   const { id } = req.params;
 
   Cafeteria
     .findByIdAndDelete(id)
-    .then(() => res.redirect("cafeteria/list_page"))
+    .then(() => res.redirect("/cafeterias"))
     .catch((err) => console.log(err));
 });
 
 
-//SHOP
+//Shop
+//Crear nueva tienda
+//GET
+router.get('/create-shop', isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
+  res.render('shop/create_form')
 
-//edit 
+});
+//POST
+router.post('/create-shop', fileUploader.single('image'), isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
 
-router.get("/edit/:id/shop", isLoggedIn, (req, res, next) => {
+  const { name, type, lat, lng, description, transport, website, rating } = req.body
+
+  Cafeteria
+    .create({ name, type, image: req.file.path, location: { coordinates: [lat, lng] }, description, transport, website, rating })
+    .then(() => res.redirect('shop/list_page'))
+    .catch(err => console.log(err))
+
+});
+
+//Editar tienda
+//GET
+router.get("/edit/:id/shop", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   const { id } = req.params;
-  Shop
+  Cafeteria
     .findById(id)
     .then((shop) => res.render("shop/edit_form", shop))
     .catch((err) => console.log(err));
 });
 
-router.post("/edit/:id/shop", isLoggedIn, fileUploader.single('image'), (req, res, next) => {
+//POST
+router.post("/edit/:id/shop", isLoggedIn, checkRole("ADMIN"), fileUploader.single('image'), (req, res, next) => {
   const { id } = req.params;
   const { name, type, lat, lng, description, transport, website, rating } = req.body;
 
-  Shop
+  Cafeteria
     .findByIdAndUpdate(id, { name, type, image: req.file.path, location: { coordinates: [lat, lng] }, description, transport, website, rating })
     .then(() => res.redirect("shop/list_page"))
     .catch((err) => console.log(err));
 });
 
-//delete
-
-router.post("/delete/:id/shop", isLoggedIn, (req, res, next) => {
+//Borrar tienda
+//POST
+router.post("/delete/:id/shop", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   const { id } = req.params;
 
-  Shop
+  Cafeteria
     .findByIdAndDelete(id)
     .then(() => res.redirect("shop/list_page"))
     .catch((err) => console.log(err));
 });
 
-module.exports = router
+module.exports = router 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -55,7 +55,7 @@ router.post('/login', (req, res, next) => {
 
 
 // Logout
-router.post('/logout', (req, res, next) => {
+router.get('/logout', (req, res, next) => {
   req.session.destroy(() => res.redirect('/login'))
 })
 
