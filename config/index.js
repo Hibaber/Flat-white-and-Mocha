c266a93d5b45a69e3fd66bd4f617c57ce631db95
Hibaber <hiba.berber@gmail.com>
2022-02-18 08:25:06 +0100

    friday

--- config/index.js
+++ config/index.js
@@ -1,8 +1,6 @@
 // We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
 
-// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
-// https://www.npmjs.com/package/morgan
 const logger = require("morgan");
 
 // ℹ️ Serves a custom favicon on each request
