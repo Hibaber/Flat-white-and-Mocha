4075441e9234b7bea9bb946c9c3548bbffd28ba1
Hibaber <hiba.berber@gmail.com>
2023-04-30 17:33:39 +0200

    updated

--- public/js/script.js
+++ public/js/script.js
@@ -7,15 +7,18 @@ document.addEventListener(
 )
 
 const nav = document.querySelector("#nav")
+const header = document.querySelector("#header")
 const open = document.querySelector("#open")
 const close = document.querySelector("#close")
 
 open.addEventListener("click", () => {
   nav.classList.add("visible")
+  header.classList.add("headerRes")
 })
 
 close.addEventListener("click", () => {
   nav.classList.remove("visible")
+  header.classList.remove("headerRes")
 })
 
 
